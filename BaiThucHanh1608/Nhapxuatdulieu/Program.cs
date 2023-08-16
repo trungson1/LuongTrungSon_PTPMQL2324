42ef462873f21bd1b48ba60794772b4d6ddb686c
trungson1 <luongtrungson01@gmail.com>
2023-08-16 10:55:40 +0700

    CreateProject

---
+++ BaiThucHanh1608/Nhapxuatdulieu/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+ {
+    private static void Main(string[] args)
+    {
+        //Console.Write("Nhap vao du lieu: ");
+        //string a = Console.ReadLine();
+        //Console.WriteLine("Du lieu nhap vao la: " + a);
+        //Khai bao bien va gan gia tri
+        const string hoten = "Luong Trung Son";
+        const int tuoi = 22;
+        Console.WriteLine("Sinh Vien: {0} ",hoten);
+        Console.WriteLine("Tuoi: {0} ",tuoi);
+
+    }
+ } 
