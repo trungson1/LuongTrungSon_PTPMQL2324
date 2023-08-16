8be178be354929ef90c28b06005a78932b405886
trungson1 <luongtrungson01@gmail.com>
2023-08-16 20:50:12 +0700

    chuyen doi du lieu Convert

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,13 +3,12 @@
     private static void Main(string[] args)
     {
         
-        string str = "123";//gia tri dau vao de chuyen doi
-        int ketqua; //bien luu ket qua sau chuyen doi
-        // bien kiem tra chuyen doi co thanh cong hay khong
-        bool KiemTra = false;
-        //chuyen doi kieu du lieu
-        KiemTra = int.TryParse(str,out(ketqua));
-        System.Console.WriteLine("ket qua = " + ketqua);
+       //Convert
+        string str = "123"; //gia tri dau vao de chuyen doi
+        //chuyen doi kieu du lieu string (gia tri = "123")
+        //sang kieu du lieu int (gia tri chuyen doi = 123)
+        int a = Convert.ToInt32(str);
+        System.Console.WriteLine("a = " + a);
         
     }
 }
