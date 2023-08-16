b3588b03796521f89494e94c9a63e5b928213c4d
trungson1 <luongtrungson01@gmail.com>
2023-08-16 20:47:18 +0700

    chuyen doi du lieu parse

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,14 +3,11 @@
     private static void Main(string[] args)
     {
         
-        Console.Write("Nhap vao du lieu: ");
-        string a = Console.ReadLine();
-        Console.WriteLine("Du lieu nhap vao la: " + a);
-        //Khai bao bien va gan gia tri
-        const string hoten = "Luong Trung Son";
-        const int tuoi = 22;
-        Console.WriteLine("Sinh Vien: {0} ",hoten);
-        Console.WriteLine("Tuoi: {0} ",tuoi);
+        //Chuyen doi du lieu
+        
+        string str = "123";
+        int a = int.Parse(str);
+        System.Console.WriteLine("a = " + a);
         
     }
 }
