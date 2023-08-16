1cff079a455d6570816d146687d52df59494c1ab
trungson1 <luongtrungson01@gmail.com>
2023-08-16 20:55:20 +0700

    cau truc re nhanh switch..case

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,24 +3,20 @@
     private static void Main(string[] args)
     {
         
-        int a,b; //khai bao 2 bien so a va b kieu int
-        //thong bao cho nguoi dung nhap vao a
-        System.Console.Write(" a = ");
-        //doc du lieu nhap tu ban phim chuyen sang kieu int va gan cho a
-        a = Convert.ToInt32(Console.ReadLine());
-        System.Console.Write(" b = ");
-        b = Convert.ToInt32(Console.ReadLine());
-        System.Console.WriteLine("cong : {0} + {1} = {2} ",a,b,a+b);
-        System.Console.WriteLine("tru : {0} - {1} = {2} ",a,b,a-b);
-        System.Console.WriteLine("nhan : {0} * {1} = {2} ",a,b,a*b);
-        System.Console.WriteLine("chia : {0} / {1} = {2} ",a,b,a/b);
-        System.Console.WriteLine("chia co du : {0} % {1} = {2} ",a,b,a%b);
-        if (a >= b) {
-				Console.WriteLine("a lon hon  b");
-			} else {
-				Console.WriteLine("a nho hon b");
-			}
-            
         
+        int day = 2;
+        switch(day)
+        {
+            case 1 : System.Console.WriteLine("Chu nhat"); break;
+            case 2 : System.Console.WriteLine("Thu 2"); break;
+            case 3 : System.Console.WriteLine("Thu 3"); break;
+            case 4 : System.Console.WriteLine("Thu 4"); break;
+            case 5 : System.Console.WriteLine("Thu 5"); break;
+            case 6 : System.Console.WriteLine("Thu 6"); break;
+            case 7 : System.Console.WriteLine("Thu 7"); break;
+            default :
+            System.Console.Write("Không phải ngày trong tuần");
+            break;
+        }
     }
 }
