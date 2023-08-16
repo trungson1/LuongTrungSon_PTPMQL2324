6cda9435a013f50d5e50fc014ab41dc14495dd9c
trungson1 <luongtrungson01@gmail.com>
2023-08-16 21:00:18 +0700

    vong lap do...while

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,13 +2,12 @@
 {
     private static void Main(string[] args)
     {
-        int a = 1;
-        while(a < 10)
+        int a = 2;
+        do
         {
-            System.Console.WriteLine("Vong lap thu {0}", a);
-            //tăng a lên 1 đơn vị để đảm bảo có lúc a >= 10
-            //a >= 10 thì vòng lặp sẽ kết thúc
+            //tang a len 1 don vi de dam bao co luc a >= 10
+            //a >= 10 thi vong lap se ket thuc
             a++;
-        }
+        }while(a < 10);
     }
 }
