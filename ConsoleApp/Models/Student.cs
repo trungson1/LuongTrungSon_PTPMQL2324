88fcf55768bb4777f21ec87b3de5b4808e290d8e
trungson1 <luongtrungson01@gmail.com>
2023-08-22 11:35:44 +0700

    student ke thua person bo sung student code

--- ConsoleApp/Models/Student.cs
+++ ConsoleApp/Models/Student.cs
@@ -1,23 +1,8 @@
 namespace ConsoleApp.Models
 {
-    public class Student
+    public class Student : Person
     {
-       public string MaSV { get; set; }
-       public string TenSV { get; set; } 
-       public int NamSinh { get; set; }
-       public void EnterData()
-       {
-        System.Console.Write("Ma SV = ");
-        MaSV = Console.ReadLine();
-        System.Console.Write("Ten SV = ");
-        TenSV = Console.ReadLine();
-        System.Console.Write("Nam Sinh = ");
-        NamSinh = Convert.ToInt16(Console.ReadLine());
-        
-       }
-        public void Display()
-       {
-        System.Console.WriteLine("Ma SV: {0} - Ten SV: {1} - Nam Sinh: {2} ",MaSV,TenSV,NamSinh);
-       }
+        public string StudentCode {get; set;}
     }
+       
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,12 +3,10 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        //khoi tao 2 doi tuong thu class Person
-        Person ps = new Person();
-        //khai bao bien va gan gia tri
-        string str = "Nguyen Van A";
-        int a = 22;
-        Console.WriteLine("{0} sinh nam {1}", str, ps.GetYearOfBirth(a));
+        Student std = new Student();
+        std.EnterData();
+        std.StudentCode = "1234567";
+        std.Display();    
     }
 }
 
