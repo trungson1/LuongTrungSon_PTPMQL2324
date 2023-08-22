69d38baf6b788104d651feb68e69b2d173d1bc22
trungson1 <luongtrungson01@gmail.com>
2023-08-22 10:51:25 +0700

    phuong thuc tinh nam sinh

--- ConsoleApp/Models/Person.cs
+++ ConsoleApp/Models/Person.cs
@@ -5,6 +5,11 @@ namespace ConsoleApp.Models
        public string FullName { get; set; }
        public string Address { get; set; } 
        public int Age { get; set; }
+       public int GetYearOfBirth (int age)
+       {
+        int yearofBirth = 2023 - age;
+        return yearofBirth;
+       }
        public void EnterData()
        {
         System.Console.Write("Full name = ");
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,18 +4,11 @@ public class Program
     private static void Main(string[] args)
     {
         //khoi tao 2 doi tuong thu class Person
-        Person ps1 = new Person();
-        Student ps2 = new Student();
-        //gan gia tri cho thuoc tinh doi tuong ps1
-        //ps1.FullName = "Nguyen Van A";
-        //ps1.Address = "Ha Noi";
-        //ps1.Age = 18;
-        ps2.MaSV = "123456";
-        ps2.TenSV= "Nguyen Van B";
-        ps2.NamSinh = 2001;
-        //goi phuong thuc hien thi thong tin
-        //ps1.Display();
-        ps2.Display();
+        Person ps = new Person();
+        //khai bao bien va gan gia tri
+        string str = "Nguyen Van A";
+        int a = 22;
+        Console.WriteLine("{0} sinh nam {1}", str, ps.GetYearOfBirth(a));
     }
 }
 
