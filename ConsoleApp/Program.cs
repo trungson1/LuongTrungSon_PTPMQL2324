2a32da6aae02cadeccb7181d5f29f3915dbd15dc
trungson1 <luongtrungson01@gmail.com>
2023-08-22 10:33:00 +0700

    goi phuong thuc nhap thong tin cho doi tuong ps2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,11 +1,21 @@
-﻿internal class Program
+﻿using ConsoleApp.Models;
+public class Program
 {
     private static void Main(string[] args)
     {
-        for(int i = 0; i < 10; i++ )
-        {
-            if(i==5) continue;
-            System.Console.WriteLine("Vong lap thu {0}", i);
-        }
+        //khoi tao 2 doi tuong thu class Person
+        Person ps1 = new Person();
+        Student ps2 = new Student();
+        //gan gia tri cho thuoc tinh doi tuong ps1
+        //ps1.FullName = "Nguyen Van A";
+        //ps1.Address = "Ha Noi";
+        //ps1.Age = 18;
+        ps2.MaSV = "123456";
+        ps2.TenSV= "Nguyen Van B";
+        ps2.NamSinh = 2001;
+        //goi phuong thuc hien thi thong tin
+        //ps1.Display();
+        ps2.Display();
     }
 }
+
