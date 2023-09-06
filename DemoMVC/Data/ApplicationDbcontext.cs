afa60cd92aef130fcdbd00eafe942737cc190b34
trungson1 <luongtrungson01@gmail.com>
2023-09-06 07:18:18 +0700

    bth0509

---
+++ DemoMVC/Data/ApplicationDbcontext.cs
@@ -0,0 +1,12 @@
+using DemoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+namespace DemoMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+        }
+        public DbSet<Student> Students {get; set;}
+    }
+}
--- DemoMVC/Program.cs
+++ DemoMVC/Program.cs
@@ -1,5 +1,9 @@
+using DemoMVC.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
-
+//set connect to database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
